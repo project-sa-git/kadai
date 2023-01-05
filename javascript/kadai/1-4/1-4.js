92a7728b1351b523e7f2db2523257e1294732bd6
mymac <trybook1901@gmail.com>
2023-01-05 22:30:18 +0900

    fix: オブジェクトを別々のkeyで管理、新規でオブジェクトをnewする形式に修正

--- javascript/kadai/1-4/1-4.js
+++ javascript/kadai/1-4/1-4.js
@@ -2,19 +2,29 @@
  * 課題①-4
  */
 
-let taskObj = {
-  掃除: "机を片付ける",
-  買い物: "牛乳を買う",
-  運動: "散歩する",
-};
-
-let i = 0;
+const tasks = [
+  {
+    content: "机を片付ける",
+    genre: "掃除",
+  },
+  {
+    content: "牛乳を買う",
+    genre: "買い物",
+  },
+  {
+    content: "運動",
+    genre: "散歩する",
+  },
+];
 
 showTaskList();
-const addValue = window.prompt("タスクを入力してください");
-const addKey = window.prompt("ジャンルを入力してください");
+const addContent = window.prompt("タスクを入力してください");
+const addGenre = window.prompt("ジャンルを入力してください");
+const newTaskObject = new Object();
+newTaskObject.content = addContent;
+newTaskObject.genre = addGenre;
 window.alert("新しいタスクを追加しました。");
-taskObj[addKey] = addValue;
+tasks.push(newTaskObject);
 showTaskList();
 const message2 = window.prompt(
   "「確認,追加,削除,終了」の４つのいずれかを入力してください"
@@ -24,9 +34,9 @@ function showTaskList() {
   console.log("=======================");
   console.log("現在持っているのタスク一覧");
   console.log("=======================");
-  for (const [key, value] of Object.entries(taskObj)) {
-    console.log(i + " : [内容]" + value + "、" + "[ジャンル]" + key);
-    i += 1;
+  for (let i in tasks) {
+    console.log(
+      i + " : [内容]" + tasks[i].content + "、" + "[ジャンル]" + tasks[i].genre
+    );
   }
-  i = 0;
 }
